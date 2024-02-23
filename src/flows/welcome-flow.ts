097e4fa94a82e8e4e0c6e0274c1c7bad5fb2a7a8
unknown <ihourquescos@gmail.com>
2024-02-23 16:07:35 -0300

    cambio emojis de SKU

--- src/flows/welcome-flow.ts
+++ src/flows/welcome-flow.ts
@@ -1,5 +1,5 @@
 const welcomeFlow = (keyword?: string) => {
-  return "Hola! Estoy para ayudarte. \nIngresa alugna de las siguientes siglas para realizar diferentes acciones: \n\n*SKU* para consulta de codigo _ej. SKU PH5548_ \n*XREF* para consultar equivalencias _ej. XREF CF9323_\n*SALDO* para consultar saldo pendiente";
+  return "Hola!! Estoy para ayudarte. \nIngresa alugna de las siguientes siglas para realizar diferentes acciones: \n\n*SKU* para consulta de codigo _ej. SKU PH5548_ \n*XREF* para consultar equivalencias _ej. XREF CF9323_\n*SALDO* para consultar saldo pendiente";
 };
 
 export default welcomeFlow;
